3ac95a6a523854303c04a13fbae32a7adf9e1ce7
Onofrei-Constantin <62692055+Onofrei-Constantin@users.noreply.github.com>
2020-05-19 18:32:34 +0300

    Initial commit

---
+++ Proiect_Forma_II/Form1.Designer.cs
@@ -0,0 +1,321 @@
+﻿namespace Proiect_Forma_II
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbAfisare = new System.Windows.Forms.RichTextBox();
+            this.txtNumeVanzator = new System.Windows.Forms.TextBox();
+            this.txtNumeCumparator = new System.Windows.Forms.TextBox();
+            this.txtTip = new System.Windows.Forms.TextBox();
+            this.txtAnFabricare = new System.Windows.Forms.TextBox();
+            this.txtDataTranzactie = new System.Windows.Forms.TextBox();
+            this.txtPret = new System.Windows.Forms.TextBox();
+            this.txtOptiuni = new System.Windows.Forms.TextBox();
+            this.txtCuloare = new System.Windows.Forms.TextBox();
+            this.btnAdauga = new System.Windows.Forms.Button();
+            this.btnAfiseaza = new System.Windows.Forms.Button();
+            this.btnCauta = new System.Windows.Forms.Button();
+            this.btnModifica = new System.Windows.Forms.Button();
+            this.lblNumeVanzator = new System.Windows.Forms.Label();
+            this.lblNumeCumparator = new System.Windows.Forms.Label();
+            this.lblTip = new System.Windows.Forms.Label();
+            this.lblAnFabricare = new System.Windows.Forms.Label();
+            this.lblDataTranzacrie = new System.Windows.Forms.Label();
+            this.lblPret = new System.Windows.Forms.Label();
+            this.lblOptiuni = new System.Windows.Forms.Label();
+            this.lblCuloare = new System.Windows.Forms.Label();
+            this.lblAdauga = new System.Windows.Forms.Label();
+            this.lblCauta = new System.Windows.Forms.Label();
+            this.lblModifica = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // rtbAfisare
+            // 
+            this.rtbAfisare.Location = new System.Drawing.Point(271, 90);
+            this.rtbAfisare.Name = "rtbAfisare";
+            this.rtbAfisare.Size = new System.Drawing.Size(331, 225);
+            this.rtbAfisare.TabIndex = 0;
+            this.rtbAfisare.Text = "";
+            // 
+            // txtNumeVanzator
+            // 
+            this.txtNumeVanzator.Location = new System.Drawing.Point(127, 12);
+            this.txtNumeVanzator.Name = "txtNumeVanzator";
+            this.txtNumeVanzator.Size = new System.Drawing.Size(100, 20);
+            this.txtNumeVanzator.TabIndex = 1;
+            // 
+            // txtNumeCumparator
+            // 
+            this.txtNumeCumparator.Location = new System.Drawing.Point(127, 38);
+            this.txtNumeCumparator.Name = "txtNumeCumparator";
+            this.txtNumeCumparator.Size = new System.Drawing.Size(100, 20);
+            this.txtNumeCumparator.TabIndex = 2;
+            // 
+            // txtTip
+            // 
+            this.txtTip.Location = new System.Drawing.Point(127, 64);
+            this.txtTip.Name = "txtTip";
+            this.txtTip.Size = new System.Drawing.Size(100, 20);
+            this.txtTip.TabIndex = 3;
+            // 
+            // txtAnFabricare
+            // 
+            this.txtAnFabricare.Location = new System.Drawing.Point(127, 90);
+            this.txtAnFabricare.Name = "txtAnFabricare";
+            this.txtAnFabricare.Size = new System.Drawing.Size(100, 20);
+            this.txtAnFabricare.TabIndex = 4;
+            // 
+            // txtDataTranzactie
+            // 
+            this.txtDataTranzactie.Location = new System.Drawing.Point(127, 116);
+            this.txtDataTranzactie.Name = "txtDataTranzactie";
+            this.txtDataTranzactie.Size = new System.Drawing.Size(100, 20);
+            this.txtDataTranzactie.TabIndex = 5;
+            // 
+            // txtPret
+            // 
+            this.txtPret.Location = new System.Drawing.Point(127, 142);
+            this.txtPret.Name = "txtPret";
+            this.txtPret.Size = new System.Drawing.Size(100, 20);
+            this.txtPret.TabIndex = 6;
+            // 
+            // txtOptiuni
+            // 
+            this.txtOptiuni.Location = new System.Drawing.Point(127, 168);
+            this.txtOptiuni.Name = "txtOptiuni";
+            this.txtOptiuni.Size = new System.Drawing.Size(100, 20);
+            this.txtOptiuni.TabIndex = 7;
+            // 
+            // txtCuloare
+            // 
+            this.txtCuloare.Location = new System.Drawing.Point(127, 194);
+            this.txtCuloare.Name = "txtCuloare";
+            this.txtCuloare.Size = new System.Drawing.Size(100, 20);
+            this.txtCuloare.TabIndex = 8;
+            // 
+            // btnAdauga
+            // 
+            this.btnAdauga.Location = new System.Drawing.Point(127, 273);
+            this.btnAdauga.Name = "btnAdauga";
+            this.btnAdauga.Size = new System.Drawing.Size(81, 28);
+            this.btnAdauga.TabIndex = 10;
+            this.btnAdauga.Text = "Adauga";
+            this.btnAdauga.UseVisualStyleBackColor = true;
+            this.btnAdauga.Click += new System.EventHandler(this.btnAdauga_Click);
+            // 
+            // btnAfiseaza
+            // 
+            this.btnAfiseaza.Location = new System.Drawing.Point(127, 307);
+            this.btnAfiseaza.Name = "btnAfiseaza";
+            this.btnAfiseaza.Size = new System.Drawing.Size(81, 28);
+            this.btnAfiseaza.TabIndex = 11;
+            this.btnAfiseaza.Text = "Afiseaza";
+            this.btnAfiseaza.UseVisualStyleBackColor = true;
+            this.btnAfiseaza.Click += new System.EventHandler(this.btnAfiseaza_Click);
+            // 
+            // btnCauta
+            // 
+            this.btnCauta.Location = new System.Drawing.Point(127, 341);
+            this.btnCauta.Name = "btnCauta";
+            this.btnCauta.Size = new System.Drawing.Size(81, 28);
+            this.btnCauta.TabIndex = 12;
+            this.btnCauta.Text = "Cauta";
+            this.btnCauta.UseVisualStyleBackColor = true;
+            this.btnCauta.Click += new System.EventHandler(this.btnCauta_Click);
+            // 
+            // btnModifica
+            // 
+            this.btnModifica.Location = new System.Drawing.Point(127, 375);
+            this.btnModifica.Name = "btnModifica";
+            this.btnModifica.Size = new System.Drawing.Size(81, 28);
+            this.btnModifica.TabIndex = 13;
+            this.btnModifica.Text = "Modifica";
+            this.btnModifica.UseVisualStyleBackColor = true;
+            this.btnModifica.Click += new System.EventHandler(this.btnModifica_Click);
+            // 
+            // lblNumeVanzator
+            // 
+            this.lblNumeVanzator.AutoSize = true;
+            this.lblNumeVanzator.Location = new System.Drawing.Point(12, 15);
+            this.lblNumeVanzator.Name = "lblNumeVanzator";
+            this.lblNumeVanzator.Size = new System.Drawing.Size(80, 13);
+            this.lblNumeVanzator.TabIndex = 14;
+            this.lblNumeVanzator.Text = "Nume Vanzator";
+            // 
+            // lblNumeCumparator
+            // 
+            this.lblNumeCumparator.AutoSize = true;
+            this.lblNumeCumparator.Location = new System.Drawing.Point(12, 41);
+            this.lblNumeCumparator.Name = "lblNumeCumparator";
+            this.lblNumeCumparator.Size = new System.Drawing.Size(92, 13);
+            this.lblNumeCumparator.TabIndex = 15;
+            this.lblNumeCumparator.Text = "Nume Cumparator";
+            // 
+            // lblTip
+            // 
+            this.lblTip.AutoSize = true;
+            this.lblTip.Location = new System.Drawing.Point(12, 67);
+            this.lblTip.Name = "lblTip";
+            this.lblTip.Size = new System.Drawing.Size(73, 13);
+            this.lblTip.TabIndex = 16;
+            this.lblTip.Text = "Firma + Model";
+            // 
+            // lblAnFabricare
+            // 
+            this.lblAnFabricare.AutoSize = true;
+            this.lblAnFabricare.Location = new System.Drawing.Point(12, 93);
+            this.lblAnFabricare.Name = "lblAnFabricare";
+            this.lblAnFabricare.Size = new System.Drawing.Size(67, 13);
+            this.lblAnFabricare.TabIndex = 17;
+            this.lblAnFabricare.Text = "An Fabricare";
+            // 
+            // lblDataTranzacrie
+            // 
+            this.lblDataTranzacrie.AutoSize = true;
+            this.lblDataTranzacrie.Location = new System.Drawing.Point(12, 119);
+            this.lblDataTranzacrie.Name = "lblDataTranzacrie";
+            this.lblDataTranzacrie.Size = new System.Drawing.Size(83, 13);
+            this.lblDataTranzacrie.TabIndex = 18;
+            this.lblDataTranzacrie.Text = "Data Tranzactie";
+            // 
+            // lblPret
+            // 
+            this.lblPret.AutoSize = true;
+            this.lblPret.Location = new System.Drawing.Point(12, 145);
+            this.lblPret.Name = "lblPret";
+            this.lblPret.Size = new System.Drawing.Size(26, 13);
+            this.lblPret.TabIndex = 19;
+            this.lblPret.Text = "Pret";
+            // 
+            // lblOptiuni
+            // 
+            this.lblOptiuni.AutoSize = true;
+            this.lblOptiuni.Location = new System.Drawing.Point(12, 171);
+            this.lblOptiuni.Name = "lblOptiuni";
+            this.lblOptiuni.Size = new System.Drawing.Size(40, 13);
+            this.lblOptiuni.TabIndex = 20;
+            this.lblOptiuni.Text = "Optiuni";
+            // 
+            // lblCuloare
+            // 
+            this.lblCuloare.AutoSize = true;
+            this.lblCuloare.Location = new System.Drawing.Point(12, 197);
+            this.lblCuloare.Name = "lblCuloare";
+            this.lblCuloare.Size = new System.Drawing.Size(43, 13);
+            this.lblCuloare.TabIndex = 21;
+            this.lblCuloare.Text = "Culoare";
+            // 
+            // lblAdauga
+            // 
+            this.lblAdauga.AutoSize = true;
+            this.lblAdauga.Location = new System.Drawing.Point(214, 281);
+            this.lblAdauga.Name = "lblAdauga";
+            this.lblAdauga.Size = new System.Drawing.Size(0, 13);
+            this.lblAdauga.TabIndex = 22;
+            // 
+            // lblCauta
+            // 
+            this.lblCauta.AutoSize = true;
+            this.lblCauta.Location = new System.Drawing.Point(214, 356);
+            this.lblCauta.Name = "lblCauta";
+            this.lblCauta.Size = new System.Drawing.Size(0, 13);
+            this.lblCauta.TabIndex = 23;
+            // 
+            // lblModifica
+            // 
+            this.lblModifica.AutoSize = true;
+            this.lblModifica.Location = new System.Drawing.Point(222, 387);
+            this.lblModifica.Name = "lblModifica";
+            this.lblModifica.Size = new System.Drawing.Size(0, 13);
+            this.lblModifica.TabIndex = 24;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblModifica);
+            this.Controls.Add(this.lblCauta);
+            this.Controls.Add(this.lblAdauga);
+            this.Controls.Add(this.lblCuloare);
+            this.Controls.Add(this.lblOptiuni);
+            this.Controls.Add(this.lblPret);
+            this.Controls.Add(this.lblDataTranzacrie);
+            this.Controls.Add(this.lblAnFabricare);
+            this.Controls.Add(this.lblTip);
+            this.Controls.Add(this.lblNumeCumparator);
+            this.Controls.Add(this.lblNumeVanzator);
+            this.Controls.Add(this.btnModifica);
+            this.Controls.Add(this.btnCauta);
+            this.Controls.Add(this.btnAfiseaza);
+            this.Controls.Add(this.btnAdauga);
+            this.Controls.Add(this.txtCuloare);
+            this.Controls.Add(this.txtOptiuni);
+            this.Controls.Add(this.txtPret);
+            this.Controls.Add(this.txtDataTranzactie);
+            this.Controls.Add(this.txtAnFabricare);
+            this.Controls.Add(this.txtTip);
+            this.Controls.Add(this.txtNumeCumparator);
+            this.Controls.Add(this.txtNumeVanzator);
+            this.Controls.Add(this.rtbAfisare);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbAfisare;
+        private System.Windows.Forms.TextBox txtNumeVanzator;
+        private System.Windows.Forms.TextBox txtNumeCumparator;
+        private System.Windows.Forms.TextBox txtTip;
+        private System.Windows.Forms.TextBox txtAnFabricare;
+        private System.Windows.Forms.TextBox txtDataTranzactie;
+        private System.Windows.Forms.TextBox txtPret;
+        private System.Windows.Forms.TextBox txtOptiuni;
+        private System.Windows.Forms.TextBox txtCuloare;
+        private System.Windows.Forms.Button btnAdauga;
+        private System.Windows.Forms.Button btnAfiseaza;
+        private System.Windows.Forms.Button btnCauta;
+        private System.Windows.Forms.Button btnModifica;
+        private System.Windows.Forms.Label lblNumeVanzator;
+        private System.Windows.Forms.Label lblNumeCumparator;
+        private System.Windows.Forms.Label lblTip;
+        private System.Windows.Forms.Label lblAnFabricare;
+        private System.Windows.Forms.Label lblDataTranzacrie;
+        private System.Windows.Forms.Label lblPret;
+        private System.Windows.Forms.Label lblOptiuni;
+        private System.Windows.Forms.Label lblCuloare;
+        private System.Windows.Forms.Label lblAdauga;
+        private System.Windows.Forms.Label lblCauta;
+        private System.Windows.Forms.Label lblModifica;
+    }
+}
+
---
+++ Proiect_Forma_II/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LibrarieModele;
+using NivelAccesDate;
+using System.Collections;
+
+namespace Proiect_Forma_II
+{
+    public partial class Form1 : Form
+    {
+        IStocareData adminMasini;
+        public Form1()
+        {
+            InitializeComponent();
+            adminMasini = StocareFactory.GetAdministratorStocare();
+        }
+
+        private void btnAdauga_Click(object sender, EventArgs e)
+        {
+            Masina s;
+            txtNumeVanzator.ForeColor = Color.Black;
+            txtNumeCumparator.ForeColor = Color.Black;
+            txtTip.ForeColor = Color.Black;
+            txtAnFabricare.ForeColor = Color.Black;
+            txtDataTranzactie.ForeColor = Color.Black;
+            txtPret.ForeColor = Color.Black;
+            txtOptiuni.ForeColor = Color.Black;
+            txtCuloare.ForeColor = Color.Black;
+            CodEroare valideaza = Validare(txtNumeVanzator.Text, txtNumeCumparator.Text, txtTip.Text, txtAnFabricare.Text, txtDataTranzactie.Text, txtPret.Text, txtOptiuni.Text, txtCuloare.Text);
+            if (valideaza != CodEroare.CORECT)
+            {
+                switch (valideaza)
+                {
+                    case CodEroare.NUMEVANZATOR_INCORECT:
+                        txtNumeVanzator.ForeColor = Color.Red;
+                        break;
+                    case CodEroare.NUMECUMPARATOR_INCORECT:
+                        txtNumeCumparator.ForeColor = Color.Red;
+                        break;
+                    case CodEroare.TIP_INCORECTE:
+                        txtTip.ForeColor = Color.Red;
+                        break;
+                    case CodEroare.ANFABRICARE_INCORECT:
+                        txtAnFabricare.ForeColor = Color.Red;
+                        break;
+                    case CodEroare.DATATRANZACTIE_INCORECT:
+                        txtDataTranzactie.ForeColor = Color.Red;
+                        break;
+                    case CodEroare.PRET_INCORECTE:
+                        txtPret.ForeColor = Color.Red;
+                        break;
+                    case CodEroare.OPTIUNE_INCORECT:
+                        txtOptiuni.ForeColor = Color.Red;
+                        break;
+                    case CodEroare.CULOARE_INCORECT:
+                        txtCuloare.ForeColor = Color.Red;
+                        break;
+                }
+            }
+            else
+            {
+                s = new Masina(txtNumeVanzator.Text, txtNumeCumparator.Text, txtTip.Text, txtAnFabricare.Text, txtDataTranzactie.Text, txtPret.Text, txtOptiuni.Text, txtCuloare.Text);
+                adminMasini.AddMasina(s);
+                lblAdauga.Text = "Masina a fost adaugata";
+            }
+        }
+        private CodEroare Validare(string numeVanzator, string numeCumparator, string Tip, string AnFabricare, string DataTranzactie, string Pret, string Optiuni, string Culoare)
+        {
+            if (numeVanzator == string.Empty)
+            {
+                return CodEroare.NUMEVANZATOR_INCORECT;
+            }
+            if (numeCumparator == string.Empty)
+            {
+                return CodEroare.NUMECUMPARATOR_INCORECT;
+            }
+            if (Tip == string.Empty)
+            {
+                return CodEroare.TIP_INCORECTE;
+            }
+            if (AnFabricare == string.Empty)
+            {
+                return CodEroare.ANFABRICARE_INCORECT;
+            }
+            if (DataTranzactie == string.Empty)
+            {
+                return CodEroare.DATATRANZACTIE_INCORECT;
+            }
+            if (Pret == string.Empty)
+            {
+                return CodEroare.PRET_INCORECTE;
+            }
+            if (Optiuni == string.Empty)
+            {
+                return CodEroare.OPTIUNE_INCORECT;
+            }
+            if (Culoare == string.Empty)
+            {
+                return CodEroare.CULOARE_INCORECT;
+            }
+            return CodEroare.CORECT;
+        }
+
+        private void btnAfiseaza_Click(object sender, EventArgs e)
+        {
+            rtbAfisare.Clear();
+            ArrayList masini = adminMasini.GetMasini();
+            foreach (Masina s in masini)
+            {
+                rtbAfisare.AppendText(s.IdMasina.ToString());
+                rtbAfisare.AppendText(" ");
+                rtbAfisare.AppendText(s.NumeVanzator);
+                rtbAfisare.AppendText(" ");
+                rtbAfisare.AppendText(s.NumeCumparator);
+                rtbAfisare.AppendText(" ");
+                rtbAfisare.AppendText(s.Tip);
+                rtbAfisare.AppendText(" ");
+                rtbAfisare.AppendText(s.AnFabricare);
+                rtbAfisare.AppendText(" ");
+                rtbAfisare.AppendText(s.DataTranzactie);
+                rtbAfisare.AppendText(" ");
+                rtbAfisare.AppendText(s.Pret);
+                rtbAfisare.AppendText(" ");
+                rtbAfisare.AppendText(s.Optiune);
+                rtbAfisare.AppendText(" ");
+                rtbAfisare.AppendText(s.Culoare);
+                rtbAfisare.AppendText(Environment.NewLine);
+            }
+        }
+
+        private void btnCauta_Click(object sender, EventArgs e)
+        {
+            Masina s = adminMasini.GetMasina(txtNumeVanzator.Text, txtTip.Text);
+            if (s != null)
+            {
+                lblCauta.Text = s.ConversiaLaSir_PentruFisier_PentruForma();
+            }
+            else
+                lblCauta.Text = "Nu s-a gasit masina";
+            if (txtNumeVanzator.Enabled == true && txtTip.Enabled == true)
+            {
+                txtNumeVanzator.Enabled = false;
+                txtTip.Enabled = false;
+            }
+            else
+            {
+                txtNumeVanzator.Enabled = true;
+                txtTip.Enabled = true;
+            }
+        }
+
+        private void btnModifica_Click(object sender, EventArgs e)
+        {
+            Masina s = adminMasini.GetMasina(txtNumeVanzator.Text, txtTip.Text);
+            if (s != null)
+            {
+
+                s.NumeCumparator=txtNumeCumparator.Text;
+                s.AnFabricare=txtAnFabricare.Text;
+                s.DataTranzactie=txtDataTranzactie.Text;
+                s.Pret=txtPret.Text;
+                s.Optiune=txtOptiuni.Text;
+                s.Culoare=txtCuloare.Text;
+
+                adminMasini.UpdateMasina(s);
+                lblModifica.Text = "Modificare efectuata";
+                txtNumeVanzator.Enabled = true;
+                txtTip.Enabled = true;
+            }
+            else
+            {
+                lblModifica.Text = "Masina inexistenta";
+            }
+            
+        }
+    }
+}
